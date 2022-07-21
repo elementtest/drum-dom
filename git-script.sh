88e2376dcaf95df6d3ce99c8c81dc5e368c91681
elementtest <elementtest7@gmail.com>
2022-07-21 20:57:58 +0800

    $input

--- git-script.sh
+++ git-script.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 #adds all files in folder to github elementtest7@gmail.com
-d -p "enter in line for git commit" input
+read -p "enter in line for git commit" input
 
 git add *
 git commit -m '$input'
