e7967fbbb3aa028faf059f56a89b9d8584d85086
elementtest <elementtest7@gmail.com>
2022-07-21 21:04:09 +0800

    gitstring

--- git-script.sh
+++ git-script.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 #adds all files in folder to github elementtest7@gmail.com
 read -p "enter in line for git commit" input
-
+gitstring=echo "$input"
 git add *
-git commit -m '$input'
+git commit -m 'gitstring'
 git push origin main
